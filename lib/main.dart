188e68da86cac1ee499c0c1d975f82ee29df8a71
Dhanush <dhanushv9060@gmail.com>
2026-07-30 00:08:20 +0530

    ADDED APP ICON

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-import 'core/theme/app_theme.dart';
-import 'features/splash/splash_screen.dart';
-import 'features/auth/login_screen.dart';
-import 'features/auth/signup_screen.dart';
-import 'features/auth/forgotpassword_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
-
-void main() {
-  runApp(const FitnessApp());
-}
-
-import 'app.dart'; // or wherever your FitnessApp widget is
+import 'core/theme/app_theme.dart';
+import 'features/auth/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
